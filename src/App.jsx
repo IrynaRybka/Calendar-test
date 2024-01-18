c87f75342bbb1e8e9b0362300ce1102412e5a4d2
IrynaRybka <irarybka@gmail.com>
2024-01-18 08:57:00 +0200

    add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
   return (
     <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <p>Calendar</p>
       <p>{import.meta.env.VITE_MESSAGING_SENDER_ID}</p>
     </>
